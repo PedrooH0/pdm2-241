ab8cb0fad13305db014654166d409de814388dd1
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2024-09-05 15:31:53 -0300

    Add files via upload

---
+++ avaliacoes/avaliacao-09/lib/screens/catalog.dart
@@ -0,0 +1,120 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:app/models/cart.dart';
+import 'package:app/models/catalog.dart';
+
+class MyCatalog extends StatelessWidget {
+  const MyCatalog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _MyAppBar(),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _MyListItem(index)),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  final Item item;
+
+  const _AddButton({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    var isInCart = context.select<CartModel, bool>(
+     
+      (cart) => cart.items.contains(item),
+    );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () {
+             
+              var cart = context.read<CartModel>();
+              cart.add(item);
+            },
+      style: ButtonStyle(
+        overlayColor: WidgetStateProperty.resolveWith<Color?>((states) {
+          if (states.contains(WidgetState.pressed)) {
+            return Theme.of(context).primaryColor;
+          }
+          return null;
+        }),
+      ),
+      child: isInCart
+          ? const Icon(Icons.check, semanticLabel: 'ADDED')
+          : const Text('ADD'),
+    );
+  }
+}
+
+class _MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      title: Text('Catalog - Pedro Henrique Lopes Sá de Farias', style: Theme.of(context).textTheme.displayLarge),
+      floating: true,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () => context.go('/catalog/cart'),
+        ),
+      ],
+    );
+  }
+}
+
+class _MyListItem extends StatelessWidget {
+  final int index;
+
+  const _MyListItem(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    var item = context.select<CatalogModel, Item>(
+      // Here, we are only interested in the item at [index]. We don't care
+      // about any other change.
+      (catalog) => catalog.getByPosition(index),
+    );
+    var textTheme = Theme.of(context).textTheme.titleLarge;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: LimitedBox(
+        maxHeight: 48,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                color: item.color,
+              ),
+            ),
+            const SizedBox(width: 24),
+            Expanded(
+              child: Text(item.name, style: textTheme),
+            ),
+            const SizedBox(width: 24),
+            _AddButton(item: item),
+          ],
+        ),
+      ),
+    );
+  }
+}
